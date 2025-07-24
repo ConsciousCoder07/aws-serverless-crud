1edbb904856abe56c9a98eb4a5ecddb69362ebc1
Faizan <faizanqazi487@gmail.com>
2025-07-24 22:04:54 +0530

    Add cognito authentication
    - Integrated OIDC authentication using react-oidc-context
    - Wrapped an authentication layer around the Home Component
    - Updated API utility functions to include access token in requests
    - Added environment variables for API and Cognito configuration
    - Updated App component to handle authentication states

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,59 +1,46 @@
-import { useEffect, useState } from "react";
-import { fetchCoffees, createCoffee } from "./utils/apis";
-import { useNavigate } from "react-router-dom";
-import "./App.css";
-import reactImg from "./assets/react.svg";
+// App.js
 
-const App = () => {
-  const [coffees, setCoffees] = useState([]);
-  const [coffeeId, setCoffeeId] = useState("");
-  const [name, setName] = useState("");
-  const [price, setPrice] = useState("");
-  const [available, setAvailable] = useState(false);
-  const navigate = useNavigate();
+import { useAuth } from "react-oidc-context";
+import Home from "./Home";
 
-  useEffect(() => {
-    fetchCoffees().then(data => setCoffees(data.Items || []));
-  }, []);
+function App() {
+  const auth = useAuth();
 
-  const handleAddCoffee = async () => {
-    if (!coffeeId || !name || !price) {
-      alert("Please fill all fields");
-      return;
-    }
-    const newCoffee = { coffeeId, name, price: Number(price), available };
-    await createCoffee(newCoffee);
-    setCoffees([...coffees, newCoffee]);
-    setCoffeeId("");
-    setName("");
-    setPrice("");
-    setAvailable(false);
+  const signOutRedirect = () => {
+    const clientId = import.meta.env.VITE_COGNITO_CLIENT;
+    const logoutUri = import.meta.env.VITE_COGNITO_LOGOUT_URI;
+    const cognitoDomain = import.meta.env.VITE_COGNITO_DOMAIN;
+    window.location.href = `${cognitoDomain}/logout?client_id=${clientId}&logout_uri=${encodeURIComponent(logoutUri)}`;
   };
 
+  if (auth.isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (auth.error) {
+    return <div>Encountering error... {auth.error.message}</div>;
+  }
+
+  if (auth.isAuthenticated) {
+    return (
+      <div>
+        {/* <pre> Hello: {auth.user?.profile.email} </pre>
+        <pre> ID Token: {auth.user?.id_token} </pre>
+        <pre> Access Token: {auth.user?.access_token} </pre>
+        <pre> Refresh Token: {auth.user?.refresh_token} </pre> */}
+        <Home />
+
+        <button onClick={() => auth.removeUser()}>Sign out</button>
+      </div>
+    );
+  }
+
   return (
-    <div className="container">
-      <h1>Coffee List</h1>
-      <div className="add-coffee-form">
-        <input className="styled-input" type="text" placeholder="Coffee ID" value={coffeeId} onChange={(e) => setCoffeeId(e.target.value)} />
-        <input className="styled-input" type="text" placeholder="Name" value={name} onChange={(e) => setName(e.target.value)} />
-        <input className="styled-input" type="number" placeholder="Price" value={price} onChange={(e) => setPrice(e.target.value)} />
-        <label>
-          <input type="checkbox" checked={available} onChange={(e) => setAvailable(e.target.checked)} /> Available
-        </label>
-        <button onClick={handleAddCoffee}>Add Coffee</button>
-      </div>
-      <div className="coffee-list">
-        {coffees.map(coffee => (
-          <div key={coffee.coffeeId} className="coffee-card" onClick={() => navigate(`/details/${coffee.coffeeId}`)}>
-            <h3>{coffee.name}</h3>
-            <img src={reactImg} alt="React Logo" />
-            <p>Price: ${coffee.price}</p>
-            <p>{coffee.available ? "Available" : "Not Available"}</p>
-          </div>
-        ))}
-      </div>
+    <div>
+      <button onClick={() => auth.signinRedirect()}>Sign in</button>
+      <button onClick={() => signOutRedirect()}>Sign out</button>
     </div>
   );
-};
+}
 
 export default App;
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,14 +4,25 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import './index.css'
 import App from './App.jsx'
 import ItemDetails from "./ItemDetails";
+import { AuthProvider } from "react-oidc-context";
+
+const cognitoAuthConfig = {
+  authority: import.meta.env.VITE_COGNITO_AUTHORITY,
+  client_id: import.meta.env.VITE_COGNITO_CLIENT_ID,
+  redirect_uri: import.meta.env.VITE_COGNITO_REDIRECT_URI,
+  response_type: "code",
+  scope: "email openid phone",
+};
 
 createRoot(document.getElementById('root')).render(
-  <Router>
-    <div>
-      <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="/details/:id" element={<ItemDetails />} />
-      </Routes>
-    </div>
-  </Router>
+  <AuthProvider {...cognitoAuthConfig}>
+    <Router>
+        <div>
+          <Routes>
+            <Route path="/" element={<App />} />
+            <Route path="/details/:id" element={<ItemDetails />} />
+          </Routes>
+        </div>
+      </Router>
+  </AuthProvider>
 )
